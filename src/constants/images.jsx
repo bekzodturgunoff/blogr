3914bff12b7640ecf694766fbcb74a61870aa15f
Bekzod <bekzodturgunoff@gmail.com>
2022-10-30 16:40:14 +0500

    testing the skills I have

---
+++ src/constants/images.jsx
@@ -0,0 +1,11 @@
+import illustration_laptop_desktop from "../assets/images/illustration-laptop-desktop.svg";
+import illustration_laptop_mobile from "../assets/images/illustration-laptop-mobile.svg";
+import illustration_editor from "../assets/images/illustration-editor-desktop.svg";
+import logo from "../assets/images/logo.svg";
+
+export default {
+  logo,
+  illustration_editor,
+  illustration_laptop_desktop,
+  illustration_laptop_mobile,
+};
---
+++ src/container/navbar/navbar.jsx
@@ -0,0 +1,38 @@
+import "./navbar.scss";
+import images from "../../constants/images";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="nav-logo-links">
+          <img src={images.logo} alt="" />
+          <ul>
+            {" "}
+            <li>
+              <a href="#">Product</a>
+            </li>
+            <li>
+              <a href="#">Company</a>
+            </li>
+            <li>
+              <a href="#">Connect</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-register">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </nav>
+      <div className="nav-bottom">
+        <h1>A modern publishing platform</h1>
+        <p>Grow your audince and build online brand</p>
+        <div className="nav-bottom-btn">
+          <button>Start for free</button>
+          <button>Learn more</button>
+        </div>
+      </div>
+    </header>
+  );
+}
