2c60c6c6ee612675e3705531e116708708639582
Bekzod <bekzodturgunoff@gmail.com>
2022-10-31 11:29:27 +0500

    some updates

--- src/container/footer/footer.jsx
+++ src/container/footer/footer.jsx
@@ -1,11 +1,61 @@
+import images from "../../constants/images";
 import "./footer.scss";
+
 export default function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <h1>Footer</h1>
-        <p>sad</p>
-        <p>deploy</p>
+        <div className="footer-logo">
+          <img src={images.logo} alt="logo" />
+        </div>
+
+        <div className="footer-links">
+          <ul>
+            <span>Product</span>
+            <li>
+              <a href="#">Overview</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Marketplace</a>
+            </li>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Integrations</a>
+            </li>
+          </ul>
+          <ul>
+            <span>Company</span>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Team</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+          </ul>
+          <ul>
+            <span>Connect</span>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Newsletter</a>
+            </li>
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
